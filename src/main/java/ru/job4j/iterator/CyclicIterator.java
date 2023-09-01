d7327ea100fbe13c3011f3df1a7595ad785981c6
Axiom <d-343@yandex.ru>
2023-09-01 21:10:15 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private List<T> data;
+
+    private int head;
+
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (head == data.size()) {
+            head = 0;
+        }
+        return head < data.size();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data.get(head++);
+    }
+}
