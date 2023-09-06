f52ac1b9cca8d05ab07c299d4e9ea9da50982d74
Axiom <d-343@yandex.ru>
2023-09-06 13:41:17 +0300

    1. Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/set/SimpleArraySet.java
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -11,8 +11,8 @@ public class SimpleArraySet<T> implements SimpleSet<T> {
 
     @Override
     public boolean add(T value) {
-        boolean rsl = false;
-        if (!contains(value)) {
+        boolean rsl = !contains(value);
+        if (rsl) {
             set.add(value);
             rsl = true;
         }
