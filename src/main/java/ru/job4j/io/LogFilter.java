22af76e14a5d87484eaf6921c8484b26d9b76c0d
Axiom <d-343@yandex.ru>
2023-09-07 16:18:46 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,9 +27,23 @@ public class LogFilter {
         return result;
     }
 
+    private void saveTo(String out) {
+        var data = filter();
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(out)))) {
+            for (String line : data) {
+                printWriter.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter("data/log.txt");
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
         logFilter.filter().forEach(System.out::println);
 
     }
+
+
 }
