8bd7455e1a9cee6172aa5c95149ec3b6ae75e241
Axiom <d-343@yandex.ru>
2023-09-08 14:25:23 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,25 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        validator(args);
+        search(Path.of(args[0]), p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validator(String[] args) {
+        if (!Files.exists(Paths.get(args[0]))) {
+            throw new IllegalArgumentException(String.format("Root folder is not correct :  %s", args[0]));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format("File extension incorrect : %s", args[1]));
+        }
     }
 }
