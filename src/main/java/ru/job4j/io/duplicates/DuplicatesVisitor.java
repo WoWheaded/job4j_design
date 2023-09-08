ee07e9fbf9283d591afd6b4d578302e3640afc07
Axiom <d-343@yandex.ru>
2023-09-08 14:00:34 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,38 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (Files.isRegularFile(file)) {
+            FileProperty current = new FileProperty(Files.size(file), file.getFileName().toString());
+            if (map.containsKey(current)) {
+                map.get(current).add(file);
+            }
+            map.putIfAbsent(current, new ArrayList<>(Arrays.asList(file)));
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    public void show() {
+        for (FileProperty fileProperty : map.keySet()) {
+            if (map.get(fileProperty).size() > 1) {
+                System.out.println("Duplicate files with size " + fileProperty.getSize() + ":");
+                for (Path path : map.get(fileProperty)) {
+                    System.out.println("Directory: " + path.getParent() + " _____ Name: " + path.getFileName() + " ____ Size: " + fileProperty.getSize());
+                }
+                System.out.println();
+            }
+        }
+    }
+}
